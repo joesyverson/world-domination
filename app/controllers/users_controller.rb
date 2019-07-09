f33175f8b0f1565fb1150d1ad8a7523f87c17f5d
Joseph G Syverson <josephgavinsyverson@gmail.com>
2019-07-09 15:56:42 -0400

    seed DB | render JSON

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,22 @@ class UsersController < ApplicationController
 
   def index
     users = User.all
-    render json: users, only: [:user_name]
+    render json: users, only: [:user_name, :pic]
+  end
+
+  def show
+    user = User.find_by(id: params[:id])
+    topics = user.topics
+    followers = topics.map {|topic| {followers: topic.followers, title: topic.title}}
+    new_followers = followers.map do |f_hsh|
+      {title: f_hsh[:title], followers_for_topic:
+        f_hsh[:followers].map do |follower|
+          follower = {id: follower.id, name: follower.name, pic: follower.pic, age: follower.age}
+        end.uniq
+      }
+    end
+    render json: {user_name: user.user_name,  pic: user.pic, followers: new_followers}
+    # render json: followers
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,14 +3,23 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#
+# Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user1 = User.create(user_name: "seline", pic: "https://farm1.static.flickr.com/79/248060017_077765ec1e.jpg")
+user_1 = User.create(user_name: "seline", pic: "https://farm1.static.flickr.com/79/248060017_077765ec1e.jpg")
 
-topic1 = Topic.create(title: "Climate Change")
+topic_1 = Topic.create(title: "Climate Change")
+topic_2 = Topic.create(title: "Vampire Registration")
 
-follower1 = Follower.create(name: "Blanche", pic: "https://cache.legacy.net/legacy/images/cobrands/GreatFallsTribune/photos/12-14obsyverson_12142010.jpg", age: 42, location: "Anytown Ohio" )
+follower_1 = Follower.create(name: "Blanche", pic: "https://cache.legacy.net/legacy/images/cobrands/GreatFallsTribune/photos/12-14obsyverson_12142010.jpg", age: 42, location: "Anytown Ohio" )
+follower_2 = Follower.create(name: "Blanche", pic: "https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fnowthisiswhatiwouldcallmusic.files.wordpress.com%2F2017%2F03%2Fnintchdbpict0003074492041.jpg&f=1", age: 42, location: "Anytown Ohio" )
 
-user1.topics << topic1
-follower1.topics << topic1
+
+
+user_1.topics << topic_1
+follower_1.topics << topic_1
+
+user_1.topics << topic_2
+follower_1.topics << topic_2
+follower_2.topics << topic_2
