f0498fbec7a446de2e677819f12521929f7f909a
Joe Jamsky <cba1067950@yahoo.com>
2019-07-09 18:01:18 -0400

    dom slapping

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,2 +1,8 @@
 class TopicsController < ApplicationController
+
+  def index
+    topics = Topic.all
+    render json: topics, only: [:title]
+  end
+
 end
--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log('goodbye world')
+
+
+//Var & Const
+
+//Event Listeners
+document.addEventListener('DOMContentLoaded', displayTopics)
+
+//Fetch
+function  fetchTopics(){
+  return fetch('http://localhost:3000/topics')
+  .then(resp => resp.json())
+}
+
+//Update DOM
+function passTopicsToDOM(){
+  // fetchTopics().then(json => displayTopics(json))
+  fetchTopics().then(slapTopicsToDOM)
+}
+
+function slapTopicsToDOM(topics){
+  const players = document.getElementsByClassName('player')
+  Array.from(players).forEach(function(player, i){
+    player.innerHTML += `<h2> Player ${i+1} </h2 >`
+
+    topics.forEach(function(topic){
+      player.innerHTML += `
+          <li> ${topic.title} </li>
+      `
+    })
+  })
+}
+
+function displayTopics(){
+  passTopicsToDOM()
+}
+
+
+//Logic
