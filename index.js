ee5f813b4307dc85a3b558ddceb780845fd1f9b3
Joe Jamsky <cba1067950@yahoo.com>
2019-07-11 13:48:18 -0400

    js basically done

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 //Var & Const
 const chart = document.getElementById('chart')
 const btn = document.querySelector('button')
+const gridder = document.querySelector('.grid-container')
+const roundsRemaining = document.querySelector('#rounds-remaining')
 btn.addEventListener('click', turnUpdate)
-let turncount = 5;
-let p1 = 5
-let p2 = 5
-let p3 = 5
+const maxTurns = 4
+let userMessage = document.querySelector('#user-message')
+
+let turncount = maxTurns
+const maxMoves = 3
+let playerMoves = {1: maxMoves, 2: maxMoves, 3: maxMoves}
 let playerPower = {1: 0, 2: 0, 3: 0}
 
 //----------------Event Listeners / Handlers
@@ -13,41 +17,24 @@ document.addEventListener('DOMContentLoaded', displayPlayers)
 
 function topicHandler(e){
   const playerID = e.target.parentElement.parentElement.parentElement.dataset.id
-  if (playerID === "1" && p1 > 0){
-    if (e.target.value === "on" ){
-      fetchCreateTopic(e)
-      p1 = p1 - 1
-      // .then(displayPlayers(json))
-      e.target.value = "off"
+  if (playerID === "1"){
+    if (playerMoves[1] > 0){
+      turnAction(e, playerID)
     } else {
-      fetchDeleteTopic(e)
-      // .then(displayPlayers(json))
-      e.target.value = "on"
+      alertUserTurnCount(e)
     }
-  } else if (playerID === "2" && p2 > 0) {
-    if (e.target.value === "on" ){
-      fetchCreateTopic(e)
-      p2 = p2 - 1
-      // .then(displayPlayers(json))
-      e.target.value = "off"
+  } else if (playerID === "2") {
+    if (playerMoves[2] > 0){
+      turnAction(e, playerID)
     } else {
-      fetchDeleteTopic(e)
-      // .then(displayPlayers(json))
-      e.target.value = "on"
+      alertUserTurnCount(e)
     }
-  } else if (playerID === "3" && p3 > 0) {
-    if (e.target.value === "on" ){
-      fetchCreateTopic(e)
-      p3 = p3 - 1
-      // .then(displayPlayers(json))
-      e.target.value = "off"
+  } else if (playerID === "3") {
+    if (playerMoves[3] > 0){
+      turnAction(e, playerID)
     } else {
-      fetchDeleteTopic(e)
-      // .then(displayPlayers(json))
-      e.target.value = "on"
+      alertUserTurnCount(e)
     }
-  } else {
-    alertUserTurnCount(e)
   }
 }
 
@@ -58,7 +45,6 @@ function fetchTopics(){
 }
 
 function fetchCreateTopic(e){
-  // debugger
   const config = {
     method: 'POST',
     headers: {'Content-Type': 'application/json'},
@@ -73,7 +59,6 @@ function fetchCreateTopic(e){
 }
 
 function fetchDeleteTopic(e){
-  // debugger
   const id = e.target.dataset.userTopicId
   const config = {
     method: "DELETE"
@@ -94,7 +79,34 @@ function getFollowers(index){
   return fetchPlayer(index)
 }
 
+function fetchJoins(){
+  return fetch("http://localhost:3000/user_topics").then((resp) => resp.json())
+}
+
+function joinIdArray(user_topics){
+  return joinArray = user_topics.map(function(user_topic){
+    return user_topic.id
+  })
+}
+
+function deleteJoins(arrayIds){
+  arrayIds.forEach(function(id){
+    fetch(`http://localhost:3000/user_topics/${id}`, {
+      method: 'DELETE'
+    })
+  })
+}
+
+function deleteJoinHandler(){
+  return fetchJoins().then((json) => joinIdArray(json)).then((array) => deleteJoins(array))
+}
+
 //----------------Update DOM-------------
+
+function talkToUser(str) {
+  userMessage.innerText = str
+}
+
 function passTopicsToDOM(){
   // fetchTopics().then(json => displayTopics(json))
   fetchTopics().then(slapTopicsToDOM)
@@ -124,6 +136,8 @@ function displayTopics(){
 }
 
 function displayPlayers(){
+  deleteJoinHandler()
+  roundsRemaining.innerText = `Rounds Remaining: ${turncount}`
   fetchPlayers().then(function(playerList){
     playerList.forEach(function(player){
 
@@ -131,11 +145,11 @@ function displayPlayers(){
       const heading = document.createElement('h2')
       const ul = document.createElement('ul')
       heading.innerHTML = `Player ${player.user_name}`
-      playerDiv.className = "players"
+      playerDiv.className = "grid-item players"
       playerDiv.dataset.id = player.id
       playerDiv.append(heading)
       playerDiv.append(ul)
-      document.body.prepend(playerDiv)
+      gridder.prepend(playerDiv)
 
     })
     displayTopics()
@@ -161,14 +175,51 @@ function displayChart(){
 
 //----------------Logic
 function turnUpdate(){
-  turncount -= 1;
-  p1 = 5
-  p2 = 5
-  p3 = 5
 
-  for (let i = 1; i < 4; i++){
-    getFollowers(i).then(function(json){
-      playerPower[i] = json.followers.length
-    }).then(() => displayChart());
+  if (turncount > 0) {
+    turncount -= 1;
+    playerMoves[1] = maxMoves
+    playerMoves[2] = maxMoves
+    playerMoves[3] = maxMoves
+
+    roundsRemaining.innerText = `Rounds Remaining: ${turncount}`
+    for (let i = 1; i < 4; i++){
+      getFollowers(i).then(function(json){
+        playerPower[i] = json.followers.length
+      }).then(() => displayChart());
+    }
+  } else {
+    gameOver()
   }
 }
+
+function turnAction(e, id){
+  if (e.target.value === "on" ){
+    fetchCreateTopic(e)
+    playerMoves[id] -= 1
+    e.target.value = "off"
+  } else {
+    fetchDeleteTopic(e)
+    e.target.value = "on"
+  }
+}
+
+function gameOver(){
+  if(playerPower[1] > playerPower[2] && playerPower[1] > playerPower[3]) {
+    talkToUser('Winner: Player 1')
+  } else if (playerPower[2] > playerPower[1] && playerPower[2] > playerPower[3]) {
+    talkToUser('Winner: Player 2')
+  } else if(playerPower[3] > playerPower[1] && playerPower[3] > playerPower[2]) {
+    talkToUser('Winner: Player 3')
+  } else if(playerPower[2] === playerPower[3] === playerPower[1]) {
+    talkToUser('Three player draw')
+  } else if (playerPower[1] === playerPower[2]) {
+    talkToUser('Draw: Player 1 and Player 2')
+  } else if (playerPower[1] === playerPower[3]) {
+    talkToUser('Draw: Player 1 and Player 3')
+  } else if (playerPower[2] === playerPower[3]) {
+    talkToUser('Draw: Player 2 and Player 3')
+  }
+}
+
+function resetGame
